f019fa512b923a07f22ba2085c7c221ef24ec514
KATHIRAVAN THILAGAR <malavikag115@gmail.com>
2024-08-24 18:46:31 -0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version ='0.0.0',
+    author= 'Malavika',
+    packages=find_packages(),
+    install_requires=[]
+)
